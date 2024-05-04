81e52fb2f253628105145e825053865a050141ae
alejandrorivero01 <167714230+alejandrorivero01@users.noreply.github.com>
2024-05-04 18:45:10 +0100

    Add files via upload
    Arquivos referentes à pasta Model de Microsoft Visual Studio

---
+++ AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LabDev.Models
+{
+    public class AppConfig
+    {
+        public string ServiceUrl = "https://personal-udj5dgyf.outsystemscloud.com/LabDev/rest/FB_DummyService/Get_Posts?AppId={0}";
+        private int AppId = 1;
+
+        public string GetServiceUrlWithId()
+        {
+            return string.Format(ServiceUrl, AppId);
+        }
+    }
+}
