6362b76430ad6c97abe36ff75e2e2445f17b5356
2303541 <2303541@estudante.uab.pt>
2024-05-05 20:52:43 +0100

    up

--- View.cs
+++ View.cs
@@ -1,26 +1,13 @@
-﻿class View
-{
-    public void ListarPostsMaisBemSucedidos(List<PostInfo> posts)
-    {
-        Console.WriteLine("Listando os posts mais bem sucedidos por visualizações:");
-        var postsOrdenados = posts.OrderByDescending(p => p.ViewCount); // Ordena os posts por número de visualizações
-        foreach (var post in postsOrdenados)
-        {
-            Console.WriteLine($"ID: {post.Id}");
-            Console.WriteLine($"AppId: {post.AppId}");
-            Console.WriteLine($"Descrição: {post.Description}");
-            Console.WriteLine($"Visualizações: {post.ViewCount}");
-            Console.WriteLine($"Likes: {post.LikeCount}");
-            Console.WriteLine($"Reações: {post.ReactionCount}");
-            Console.WriteLine($"Data Criação: {post.CreationDate}");
-            Console.WriteLine();
-        }
+﻿using System;
 
-        public void ListarPostsMenosBemSucedidos(List<PostInfo> posts)
+namespace LabDev
+{
+    class View
+    {
+
+        private void DisplayData(List<PostInfo> posts)
         {
-            Console.WriteLine("Listando os posts menos bem sucedidos por visualizações:");
-            var postsOrdenados = posts.OrderBy(p => p.ViewCount); // Ordena os posts por número de visualizações em ordem crescente
-            foreach (var post in postsOrdenados)
+            foreach (var post in posts)
             {
                 Console.WriteLine($"ID: {post.Id}");
                 Console.WriteLine($"AppId: {post.AppId}");
@@ -32,20 +19,26 @@
                 Console.WriteLine();
             }
         }
-        public void ListarPostsComMaisReacoes(List<PostInfo> posts)
+        public void ListarPostsMaisBemSucedidos(List<PostInfo> posts)
         {
-            Console.WriteLine("Listando os posts com mais reações:");
-            var postsOrdenados = posts.OrderByDescending(p => p.ReactionCount); // Ordena os posts por número de reações em ordem decrescente
-            foreach (var post in postsOrdenados)
             {
-                Console.WriteLine($"ID: {post.Id}");
-                Console.WriteLine($"AppId: {post.AppId}");
-                Console.WriteLine($"Descrição: {post.Description}");
-                Console.WriteLine($"Visualizações: {post.ViewCount}");
-                Console.WriteLine($"Likes: {post.LikeCount}");
-                Console.WriteLine($"Reações: {post.ReactionCount}");
-                Console.WriteLine($"Data Criação: {post.CreationDate}");
-                Console.WriteLine();
+                Console.WriteLine("Listando os posts mais bem sucedidos por visualizações:");
+                DisplayData(posts);
+            }
+
+            public void ListarPostsMenosBemSucedidos(List<PostInfo> posts)
+            {
+                {
+                    Console.WriteLine("Listando os posts menos bem sucedidos por visualizações:");
+                    DisplayData(posts);
+                }
+            }
+            public void ListarPostsComMaisReacoes(List<PostInfo> posts)
+            {
+                {
+                    Console.WriteLine("Listando os posts com mais reações:");
+                    DisplayData(posts);
+                }
             }
         }
     }
